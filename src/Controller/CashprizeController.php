4ca58e833a2156494773134d40f900856c11907e
jonathan jorand <jjorand@ankama.com>
2018-12-20 13:22:36 +0100

    fix rules and cashprize

---
+++ src/Controller/CashprizeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class CacheprizeController
+{
+    /**
+     * @Route("/", name="cashprize")
+     * @param Environment $twig
+     * @return Response
+     */
+    public function index(Environment $twig)
+    {
+        return new Response(
+            $twig->render('cashprize/index.html.twig')
+        );
+    }
+}
