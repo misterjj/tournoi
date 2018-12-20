b1f8fc7f5f6bbd075daabdd61a77e1516ffea0d5
jonathan jorand <jjorand@ankama.com>
2018-12-20 13:25:30 +0100

    fix rules and cashprize

--- src/Controller/CashprizeController.php
+++ src/Controller/CashprizeController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
-class CacheprizeController
+class CashprizeController
 {
     /**
      * @Route("/", name="cashprize")
